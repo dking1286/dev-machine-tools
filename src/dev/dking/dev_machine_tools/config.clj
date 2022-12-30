52091e7451665cc5dab922f1f33fe4a89d64ce5b
Daniel King <daniel.oliver.king@gmail.com>
2022-12-30 10:37:40 -0800

    Add a function to check if the dev-machine exists.

--- src/dev/dking/dev_machine_tools/config.clj
+++ src/dev/dking/dev_machine_tools/config.clj
@@ -4,6 +4,6 @@
   []
   {:platform {:type :gce
               :project-id "atelier-royal"}
-   :dev-machine-name "dev-machine"})
+   :name "dev-machine"})
 
 (def get-config (memoize get-config-impl))
--- src/dev/dking/dev_machine_tools/gce.clj
+++ src/dev/dking/dev_machine_tools/gce.clj
@@ -1,6 +1,7 @@
 (ns dev.dking.dev-machine-tools.gce
   (:require [babashka.process :refer [sh]]
             [cheshire.core :as json]
+            [clojure.string :as string]
             [dev.dking.dev-machine-tools.interface :refer [running? start stop]]))
 
 (def status {:terminated "TERMINATED"
@@ -17,16 +18,45 @@
       (json/parse-string-strict out)
       (throw (ex-info err {:type :gcloud-error})))))
 
+(defn- describe
+  [config]
+  (gcloud config "compute" "instances" "describe" (config :name)))
+
+(defn- exists?
+  [config]
+  (try
+    (describe config)
+    true
+    (catch clojure.lang.ExceptionInfo e
+      (if (string/includes? (ex-message e) "Could not fetch resource")
+        false
+        (throw e)))))
+
 (defmethod running? :gce
   [config]
-  (let [current-status (-> (gcloud config "compute" "instances" "describe" (config :dev-machine-name))
+  (let [current-status (-> (describe config)
                            (get "status"))]
     (= current-status (status :running))))
 
 (defmethod start :gce
   [config]
-  (gcloud config "compute" "instances" "start" (config :dev-machine-name)))
+  (gcloud config "compute" "instances" "start" (config :name)))
 
 (defmethod stop :gce
   [config]
-  (gcloud config "compute" "instances" "stop" (config :dev-machine-name)))
+  (gcloud config "compute" "instances" "stop" (config :name)))
+
+(comment
+  (require '[dev.dking.dev-machine-tools.config :refer [get-config get-config-impl]])
+
+  (def config (get-config-impl))
+
+  config
+
+  (gcloud config "compute" "instances" "describe" "i-dont-exist")
+  (sh "gcloud" "compute" "instances" "describe" "i-dont-exist" "--project=atelier-royal" "--format=json")
+
+  (describe config)
+
+  (exists? config)
+  (exists? (assoc config :name "i-dont-exist")))
