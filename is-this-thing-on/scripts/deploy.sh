6cf604df67d1c798187be15c3a5a71dce78fa28a
Daniel King <daniel.oliver.king@gmail.com>
2023-08-11 14:21:14 +0000

    Remove golang version and rename Clojure version to is-this-thing-on

--- is-this-thing-on/scripts/deploy.sh
+++ is-this-thing-on/scripts/deploy.sh
@@ -1,6 +1,9 @@
+clj -T:build uberjar
+
 gcloud functions deploy is_this_thing_on \
   --gen2 \
   --region="us-central1" \
-  --runtime="go120" \
-  --entry-point="IsThisThingOn" \
+  --runtime="java17" \
+  --source="target/deployment" \
+  --entry-point="dev.dking.dev_machine_tools.is_this_thing_on.CloudFunction" \
   --trigger-topic="is-this-thing-on"
