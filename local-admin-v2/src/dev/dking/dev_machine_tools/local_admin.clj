40b0b62effb74f5159a080402a65e364cc651bcd
Daniel King <daniel.oliver.king@gmail.com>
2023-11-24 10:23:31 -0800

    Add a down function to stop the instance.

--- local-admin-v2/src/dev/dking/dev_machine_tools/local_admin.clj
+++ local-admin-v2/src/dev/dking/dev_machine_tools/local_admin.clj
@@ -1,7 +1,10 @@
 (ns dev.dking.dev-machine-tools.local-admin
   (:require [clojure.data.json :as json])
-  (:import [com.google.cloud.compute.v1 InstancesClient ListInstancesRequest StartInstanceRequest]
+  (:import [com.google.cloud.compute.v1 InstancesClient ListInstancesRequest StartInstanceRequest StopInstanceRequest]
            [com.google.protobuf.util JsonFormat]))
+
+(def status {:running "RUNNING"
+             :terminated "TERMINATED"})
 
 (defn- unsafe-proto->edn
   [proto]
@@ -16,38 +19,91 @@
     (unsafe-proto->edn proto)))
 
 (defn- get-instance
-  [client]
+  [client {:keys [project zone instance-name]}]
   (let [list-instances-request (-> (ListInstancesRequest/newBuilder)
-                                   (.setProject "atelier-royal")
-                                   (.setZone "us-central1-a")
-                                   (.setFilter (format "name=%s" "dev-machine"))
+                                   (.setProject project)
+                                   (.setZone zone)
+                                   (.setFilter (format "name=%s" instance-name))
                                    (.build))
         list-instances-response (.list client list-instances-request)
         instance (first (.iterateAll list-instances-response))]
     (proto->edn instance)))
 
 (defn- start-instance!
-  [client]
+  [client {:keys [project zone instance-name]}]
   (let [request (-> (StartInstanceRequest/newBuilder)
-                    (.setProject "atelier-royal")
-                    (.setZone "us-central1-a")
-                    (.setInstance "dev-machine")
+                    (.setProject project)
+                    (.setZone zone)
+                    (.setInstance instance-name)
                     (.build))
         result-future (.startAsync client request)
         result (.get result-future)]
     (proto->edn result)))
 
+(defn- stop-instance!
+  [client {:keys [project zone instance-name]}]
+  (let [request (-> (StopInstanceRequest/newBuilder)
+                    (.setProject project)
+                    (.setZone zone)
+                    (.setInstance instance-name)
+                    (.build))
+        result-future (.stopAsync client request)
+        result (.get result-future)]
+    (proto->edn result)))
+
+(defn- up-impl
+  []
+  (with-open [instances-client (InstancesClient/create)]
+    (let [project "atelier-royal"
+          zone "us-central1-a"
+          instance-name "dev-machine"
+          instance (get-instance instances-client
+                                 {:project project
+                                  :zone zone
+                                  :instance-name instance-name})]
+      (if-not instance
+        (println "No instance found.")
+        (if (= (instance :status) (status :running))
+          (println "Instance is already running, no need to start it.")
+          (do
+            (println "Starting instance...")
+            (start-instance! instances-client
+                             {:project project
+                              :zone zone
+                              :instance-name instance-name})
+            (println "Instance started!")))))))
+
+(defn- down-impl
+  []
+  (with-open [instances-client (InstancesClient/create)]
+    (let [project "atelier-royal"
+          zone "us-central1-a"
+          instance-name "dev-machine"
+          instance (get-instance instances-client
+                                 {:project project
+                                  :zone zone
+                                  :instance-name instance-name})]
+      (if-not instance
+        (println "No instance found.")
+        (if (= (instance :status) (status :terminated))
+          (println "Instance is already terminated, no need to stop it.")
+          (do
+            (println "Stopping instance...")
+            (stop-instance! instances-client
+                            {:project project
+                             :zone zone
+                             :instance-name instance-name})
+            (println "Instance stopped!")))))))
+
 (defn up
   [& _]
-  (let [instances-client (InstancesClient/create)
-        instance (get-instance instances-client)]
-    (if-not instance
-      (println "No instance found.")
-      (if (= (:status instance) "RUNNING")
-        (println "Instance is already running, no need to start it.")
-        (do
-          (println "Starting instance...")
-          (start-instance! instances-client))))))
+  (up-impl)
+  (System/exit 0))
+
+(defn down
+  [& _]
+  (down-impl)
+  (System/exit 0))
 
 (comment
   (require '[clojure.reflect :refer [reflect]])
@@ -79,5 +135,18 @@
       (.getErrorsList))
   (proto->edn nil)
 
-  (up))
+  (up-impl)
+
+  (get-instance client {:project "atelier-royal"
+                        :zone "us-central1-a"
+                        :instance-name "dev-machine"})
+
+  (let [request (-> (StopInstanceRequest/newBuilder)
+                    (.setProject "atelier-royal")
+                    (.setZone "us-central1-a")
+                    (.setInstance "dev-machine")
+                    (.build))
+        result-future (.stopAsync client request)
+        result (.get result-future)]
+    (proto->edn result)))
 
