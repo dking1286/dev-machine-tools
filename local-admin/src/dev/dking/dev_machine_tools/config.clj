e0d8dc21c9025aa609f881d73c59fc284c7a8e68
Daniel King <daniel.oliver.king@gmail.com>
2024-01-27 09:13:26 -0800

    Replace old local-admin folder with the new one.

--- local-admin/src/dev/dking/dev_machine_tools/config.clj
+++
@@ -1,9 +0,0 @@
-(ns dev.dking.dev-machine-tools.config)
-
-(defn get-config-impl
-  []
-  {:platform {:type :gce
-              :project-id "atelier-royal"}
-   :name "dev-machine"})
-
-(def get-config (memoize get-config-impl))
